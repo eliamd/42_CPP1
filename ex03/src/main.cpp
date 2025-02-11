9fabedc62110ccdaad7b831c05610f28cae98bc9
Eliam <Texsword76@gmail.com>
2025-02-11 18:03:48 +0100

    Ajout d'emojis aux noms des armes et des personnages dans main.cpp pour une meilleure lisibilité.

--- ex03/src/main.cpp
+++ ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: edetoh <edetoh@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 15:04:41 by edetoh            #+#    #+#             */
-/*   Updated: 2025/02/11 17:36:09 by edetoh           ###   ########.fr       */
+/*   Updated: 2025/02/11 18:02:53 by edetoh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,31 @@
 
 int main()
 {
-	Weapon AK_47("Kalash");
-	Weapon P90("P90");
-	Weapon PP_Bizon("P90");
-	Weapon Famas("Famas");
+	Weapon AK_47("🔫 Kalash");
+	Weapon P90("🔫 P90");
+	Weapon PP_Bizon("🔫 PP_Bizon");
+	Weapon Famas("🔫 Famas");
 
-	HumanA human1(AK_47, "Mehdi");
-	HumanA human2(P90, "Edwin");
+	HumanA human1(AK_47, "👨🏼 Mehdi");
+	HumanA human2(P90, "🧑🏿 Edwin");
 
-	HumanB human3("Lola");
-	HumanB human4("Octa");
+	HumanB human3("👧🏻 Lola");
+	HumanB human4("🧓🏻 Octa");
+
+	human1.attack();
+	human2.attack();
+	human3.attack();
+	human4.attack();
+
+	human3.set_weapon(PP_Bizon);
+	human4.set_weapon(Famas);
+
+	std::cout << "--------------------------" << std::endl;
+
+	human1.attack();
+	human2.attack();
+	human3.attack();
+	human4.attack();
+
 	return 0;
 }
